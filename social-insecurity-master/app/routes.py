ea4e8063b19979be80dec532d675afe1f6dfd203
Magnus Egeland <magnus.egeland@gmail.com>
2022-09-23 12:20:54 +0200

    added password hashing to database

--- social-insecurity-master/app/routes.py
+++ social-insecurity-master/app/routes.py
@@ -3,6 +3,7 @@ from app import app, query_db
 from app.forms import IndexForm, PostForm, FriendsForm, ProfileForm, CommentsForm
 from datetime import datetime
 import os
+from werkzeug.security import generate_password_hash,check_password_hash
 
 # this file contains all the different routes, and the logic for communicating with the database
 
@@ -15,15 +16,15 @@ def index():
     if form.login.is_submitted() and form.login.submit.data:
         user = query_db('SELECT * FROM Users WHERE username="{}";'.format(form.login.username.data), one=True)
         if user == None:
-            flash('Sorry, this user does not exist!')
-        elif user['password'] == form.login.password.data:
+            flash('Wrong username or password!')
+        elif check_password_hash(user['password'],  form.login.password.data):
             return redirect(url_for('stream', username=form.login.username.data))
         else:
-            flash('Sorry, wrong password!')
+            flash('Wrong username or password!')
 
     elif form.register.is_submitted() and form.register.submit.data:
         query_db('INSERT INTO Users (username, first_name, last_name, password) VALUES("{}", "{}", "{}", "{}");'.format(form.register.username.data, form.register.first_name.data,
-         form.register.last_name.data, form.register.password.data))
+         form.register.last_name.data, generate_password_hash(form.register.password.data,"sha256")))
         return redirect(url_for('index'))
     return render_template('index.html', title='Welcome', form=form)
 
