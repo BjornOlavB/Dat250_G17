8c121a21f4e41560adee0cd68c0fd3f7e74791b1
Hakurem <89521376+Hakurem@users.noreply.github.com>
2022-09-24 17:51:25 +0200

    changing every single {} to ? to sanitize inputs from the user into the query of the dbms

--- social-insecurity-master/app/routes.py
+++ social-insecurity-master/app/routes.py
@@ -14,7 +14,8 @@ import time
 def my_validation(form_type):
     form_type.validate_on_submit() # whos validation errors
     errs = form_type.errors
-    if len(errs) == 1 and "csrf_token" in errs:
+    print(errs)
+    if len(errs) == 0 or ("csrf_token" in errs and len(errs) == 1):
         return True
     else:
         return False
@@ -30,7 +31,7 @@ def index():
     
 
     if form.login.is_submitted() and form.login.submit.data and my_validation(form.login):
-        user = query_db('SELECT * FROM Users WHERE username="{}";'.format(form.login.username.data), one=True)
+        user = query_db('SELECT * FROM Users WHERE username=?;',form.login.username.data, one=True)
         # Could not find user
         if user == None:
             flash('Wrong username or password!')
@@ -49,10 +50,10 @@ def index():
 
         # Restore the login_attemps and login_timeout when the time is over
         if time_left <= 0 and user["login_timeout"] != None:
-            query_db('UPDATE Users \
-                    SET login_attempts = 0, login_timeout = NULL\
-                    WHERE id = "{}";'.format(user["id"]), one=True)
-            user = query_db('SELECT * FROM Users WHERE username="{}";'.format(user["id"]), one=True)
+            query_db("""UPDATE Users 
+                    SET login_attempts = 0, login_timeout = NULL
+                    WHERE id = ?;""",user["id"], one=True)
+            user = query_db('SELECT * FROM Users WHERE username=?;',user["id"], one=True)
         
         # redirect and flash that the user is locked out 
         elif time_left > 0:
@@ -62,6 +63,11 @@ def index():
 
         # User successfully logged in
         if check_password_hash(user['password'],  form.login.password.data):
+            # Reset the login_attemps and login_timeout
+            query_db("""UPDATE Users 
+                    SET login_attempts = 0, login_timeout = NULL
+                    WHERE id = ?;""",user["id"], one=True)
+
             res = make_response(redirect(url_for('stream', username=form.login.username.data)))
             res.set_cookie("username",form.login.username.data)
             return res
@@ -71,21 +77,21 @@ def index():
         elif not check_password_hash(user["Password"], form.login.password.data):
             
             if user["Login_attempts"] < 2:
-                query_db('UPDATE Users SET login_attempts = login_attempts + 1 WHERE id = "{}";'.format(user["id"]), one=True)
+                query_db('UPDATE Users SET login_attempts = login_attempts + 1 WHERE id = ?;',user["id"], one=True)
                 flash('Wrong username or password!')
 
             elif user["Login_attempts"] == 2:
                 lockout_time = 5 # minutes
                 lockout_stamp = time.time() + lockout_time*60 # minutes
                 flash(f'You have been locked out of your account for {lockout_time} minutes due to too many failed login attempts')
-                query_db('UPDATE Users SET login_timeout = "{}" WHERE id = "{}";'.format(lockout_stamp,user["id"]), one=True)
+                query_db('UPDATE Users SET login_timeout = ? WHERE id = ?;',lockout_stamp,user["id"], one=True)
         #_________________________________
 
         else:
             flash('Unknown error')
 
     elif form.register.is_submitted() and form.register.submit.data and my_validation(form.register):
-            user = query_db('SELECT * FROM Users WHERE username="{}";'.format(form.register.username.data), one=True)
+            user = query_db('SELECT * FROM Users WHERE username=?;',form.register.username.data,one=True)
             if user != None:
                 flash('Username is already taken!')
             else:
@@ -96,11 +102,11 @@ def index():
                         last_name, 
                         password
                         ) 
-                        VALUES("{}","{}","{}","{}");"""
-                .format(form.register.username.data, 
+                        VALUES(?,?,?,?);""",
+                        form.register.username.data, 
                         form.register.first_name.data,
                         form.register.last_name.data, 
-                        generate_password_hash(form.register.password.data,"sha256")))
+                        generate_password_hash(form.register.password.data,"sha256"))
 
                 flash(f'account {form.register.username.data} was successfully created')
                 return redirect(url_for('index'))
@@ -111,20 +117,22 @@ def index():
 def stream(username):
     form = PostForm()
 
-    # if the user is not logged in, redirect to the login page
+    user = query_db('SELECT * FROM Users WHERE username=?;',username, one=True)
+
+    # if the user is not logged in or doesn't exist, redirect to the login page
     coockieUsername = request.cookies.get("username")
     if coockieUsername != username:
-        return redirect(url_for('stream', username=coockieUsername))
-        #return redirect(url_for("index"))
+        return redirect(url_for('index'))
+    elif user == None:
+        flash("User does not exist")
+        return redirect(url_for('index'))
 
-    # if the user is logged in, show the stream
-    user = query_db('SELECT * FROM Users WHERE username="{}";'.format(username), one=True)
 
     if form.is_submitted() and form.submit.data and my_validation(form):
 
-        # Remove all the newlines and carriage returns from the content of the post
+        # remove "\r" and "\n" and <"> from the content of the post escape the content
         content = form.content.data.replace("\n", "").replace("\r", "")
-    
+
         if form.image.data:
             path = os.path.join(app.config['UPLOAD_PATH'], form.image.data.filename)
             form.image.data.save(path)
@@ -135,7 +143,8 @@ def stream(username):
                     image,
                     creation_time
                     ) 
-                    VALUES("{}","{}","{}","{}");""".format(user['id'], form.content.data, form.image.data.filename, datetime.now()))
+                    VALUES(?, ?, ?, ?);""",
+                    user['id'], form.content.data, form.image.data.filename, datetime.now())
         
         elif form.content.data:
             query_db(
@@ -144,14 +153,23 @@ def stream(username):
                     content, 
                     creation_time
                     ) 
-                    VALUES("{}","{}","{}");""".format(user['id'], content, datetime.now()))
+                    VALUES(?,?,?);""",
+                    user['id'], content, datetime.now())
         else:
             flash('You must enter some content or upload an image')
 
-        #query_db('INSERT INTO Posts (u_id, content, image, creation_time) VALUES({}, "{}", "{}", \'{}\');'.format(user['id'], form.content.data, form.image.data.filename, datetime.now()))
+       
         return redirect(url_for('stream', username=username))
     
-    posts = query_db('SELECT p.*, u.*, (SELECT COUNT(*) FROM Comments WHERE p_id=p.id) AS cc FROM Posts AS p JOIN Users AS u ON u.id=p.u_id WHERE p.u_id IN (SELECT u_id FROM Friends WHERE f_id={0}) OR p.u_id IN (SELECT f_id FROM Friends WHERE u_id={0}) OR p.u_id={0} ORDER BY p.creation_time DESC;'.format(user['id']))
+    posts = query_db("""SELECT p.*, u.*, (SELECT COUNT(*) 
+                        FROM Comments 
+                        WHERE p_id=p.id) AS cc 
+                        FROM Posts AS p 
+                        JOIN Users AS u ON u.id=p.u_id 
+                        WHERE p.u_id IN (SELECT u_id FROM Friends WHERE f_id=?) 
+                        OR p.u_id IN (SELECT f_id FROM Friends WHERE u_id=?) OR p.u_id=?
+                        ORDER BY p.creation_time DESC;""",user['id'], user['id'], user['id'])
+                        
     return render_template('stream.html', title='Stream', username=username, form=form, posts=posts)
 
 
@@ -164,11 +182,14 @@ def comments(username, p_id):
         return redirect(url_for("index"))
     form = CommentsForm()
     if form.is_submitted():
-        user = query_db('SELECT * FROM Users WHERE username="{}";'.format(username), one=True)
-        query_db('INSERT INTO Comments (p_id, u_id, comment, creation_time) VALUES({}, {}, "{}", \'{}\');'.format(p_id, user['id'], form.comment.data, datetime.now()))
+        user = query_db('SELECT * FROM Users WHERE username=?;', username,one=True)
+        query_db('INSERT INTO Comments (p_id, u_id, comment, creation_time) VALUES(?,?,?,?);',p_id, user['id'], form.comment.data, datetime.now())
 
-    post = query_db('SELECT * FROM Posts WHERE id={};'.format(p_id), one=True)
-    all_comments = query_db('SELECT DISTINCT * FROM Comments AS c JOIN Users AS u ON c.u_id=u.id WHERE c.p_id={} ORDER BY c.creation_time DESC;'.format(p_id))
+    post = query_db('SELECT * FROM Posts WHERE id=?;',p_id, one=True)
+    all_comments = query_db("""SELECT DISTINCT * FROM Comments 
+                            AS c JOIN Users AS u ON c.u_id=u.id 
+                            WHERE c.p_id=? ORDER BY c.creation_time DESC;""",p_id)
+
     return render_template('comments.html', title='Comments', username=username, form=form, post=post, comments=all_comments)
 
 # page for seeing and adding friends
@@ -178,15 +199,17 @@ def friends(username):
     if coockieUsername != username:
         return redirect(url_for("index"))
     form = FriendsForm()
-    user = query_db('SELECT * FROM Users WHERE username="{}";'.format(username), one=True)
+    user = query_db('SELECT * FROM Users WHERE username=?;',username, one=True)
     if form.is_submitted():
-        friend = query_db('SELECT * FROM Users WHERE username="{}";'.format(form.username.data), one=True)
+        friend = query_db('SELECT * FROM Users WHERE username=?;',form.username.data, one=True)
         if friend is None:
             flash('User does not exist')
         else:
-            query_db('INSERT INTO Friends (u_id, f_id) VALUES({}, {});'.format(user['id'], friend['id']))
+            query_db('INSERT INTO Friends (u_id, f_id) VALUES(?,?);',user['id'], friend['id'])
     
-    all_friends = query_db('SELECT * FROM Friends AS f JOIN Users as u ON f.f_id=u.id WHERE f.u_id={} AND f.f_id!={} ;'.format(user['id'], user['id']))
+    all_friends = query_db("""SELECT * FROM Friends 
+                            AS f JOIN Users as u ON f.f_id=u.id 
+                            WHERE f.u_id=? AND f.f_id!=? ;""",user['id'], user['id'])
     return render_template('friends.html', title='Friends', username=username, friends=all_friends, form=form)
 
 # see and edit detailed profile information of a user
@@ -197,12 +220,16 @@ def profile(username):
         return redirect(url_for("index"))
     form = ProfileForm()
     if form.is_submitted():
-        query_db('UPDATE Users SET education="{}", employment="{}", music="{}", movie="{}", nationality="{}", birthday=\'{}\' WHERE username="{}" ;'.format(
-            form.education.data, form.employment.data, form.music.data, form.movie.data, form.nationality.data, form.birthday.data, username
-        ))
+
+        query_db("""UPDATE Users SET education=?, employment="?, music=?, 
+                    movie=?, nationality=?, birthday=? WHERE username=? ;""",
+                    form.education.data, form.employment.data, form.music.data, 
+                    form.movie.data, form.nationality.data, form.birthday.data, username
+                    )
+        
         return redirect(url_for('profile', username=username))
     
-    user = query_db('SELECT * FROM Users WHERE username="{}";'.format(username), one=True)
+    user = query_db('SELECT * FROM Users WHERE username=?;',username, one=True)
     return render_template('profile.html', title='profile', username=username, user=user, form=form)
 
 @app.route('/logout')
